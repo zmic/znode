8677e6c2bf29b7e46b5a4bb799083633b6d551c2
michael <not.here@gmail.com>
2020-05-25 13:33:19 +0200

    xxx

---
+++ znode/extra.py
@@ -0,0 +1,36 @@
+import numpy as np
+import randomgen
+
+from .node__ import node____, node_literal__, node__, metaclass_node_apply__
+from .nodes import node_numpy__, node_dict
+
+#-------------------------------------------------------    
+def in_node_dict(x):
+    node_dict[x.__name__] = x
+    return x
+
+@in_node_dict
+class ŋcanvas(node_numpy__):
+    @staticmethod
+    def eval__(X, Y, x0, x1, y0, y1, endpoint, dtype):
+        I = np.indices((Y,X)).astype(np.float64)
+        fy = (y1 - y0) / (Y - 1) if endpoint else (y1 - y0) / Y
+        fx = (x1 - x0) / (X - 1) if endpoint else (x1 - x0) / X
+        I[0], I[1] = x0 + fx*I[1], y0 + fy*I[0]
+        return I.astype(dtype)
+
+@in_node_dict
+class ŋccanvas(node_numpy__):
+    @staticmethod
+    def eval__(X, Y, x0, x1, y0, y1, endpoint, dtype):
+        I = np.indices((Y,X)).astype(dtype)
+        fy = (y1 - y0) / (Y - 1) if endpoint else (y1 - y0) / Y
+        fx = (x1 - x0) / (X - 1) if endpoint else (x1 - x0) / X
+        return = (x0 + fx*I[1]) + 1j*(y0 + fy*I[0])
+
+@in_node_dict
+class ŋrotatexy(node_numpy__):
+    @staticmethod
+    def eval__(X, Y, a):
+        return X*np.cos(a)+Y*np.sin(a), -X*np.sin(a)+Y*np.cos(a)
+
